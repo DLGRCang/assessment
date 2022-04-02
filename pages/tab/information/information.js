180453688fc23614fae7e456e3ed461b2489cf25
DeLiGeRiCang <2533678748@qq.com>
2022-04-02 10:30:42 +0800

    bt

---
+++ pages/tab/information/information.js
@@ -0,0 +1,78 @@
+// pages/tab/information/information.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {id:1,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:2,text:"工作外派",image:"../../../image/inform/1.png",url:""},
+      {id:3,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:4,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:5,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:6,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:7,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:8,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:9,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:10,text:"驻村",image:"../../../image/inform/1.png",url:""},
+      {id:11,text:"驻村",image:"../../../image/inform/1.png",url:""},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
